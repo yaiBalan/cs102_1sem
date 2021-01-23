195e3c028ebe2e0f1ed96215feeb6b0bd16150b1
yaiBalan <imexikashka633@gmail.com>
2021-01-23 13:06:27 +0300

    fix

--- exam-winter/map.py
+++ exam-winter/map.py
@@ -1,8 +1,8 @@
 """
 Sam Bridges task solution
 """
-import typing as tp
 import pathlib
+import typing as tp
 
 
 def load_map(mapfile: tp.Union[pathlib.Path, str] = "default.map") -> tp.List[tp.List[str]]:
@@ -13,22 +13,29 @@ def load_map(mapfile: tp.Union[pathlib.Path, str] = "default.map") -> tp.List[tp
 
     return map
 
-def find_path(map, sam = None):
+
+def find_path(map, sam=None):
     if not sam:
         for i in range(len(map)):
             for j in range(len(map[i])):
                 if map[i][j] == "☺":
                     sam = (i, j)
-    
-    possible_moves = [(sam[0]-1, sam[1]), (sam[0]+1, sam[1]), (sam[0], sam[1]-1), (sam[0], sam[1]+1)]
+
+    possible_moves = [
+        (sam[0] - 1, sam[1]),
+        (sam[0] + 1, sam[1]),
+        (sam[0], sam[1] - 1),
+        (sam[0], sam[1] + 1),
+    ]
 
     for move in possible_moves:
         if map[move[0]][move[1]] == ".":
             map[move[0]][move[1]] = "☺"
             sam = move
             find_path(map, sam)
-     
+
     return map
+
 
 def reconstruct_map(map: tp.List[tp.List[str]]) -> str:
     list_map = []
@@ -36,7 +43,7 @@ def reconstruct_map(map: tp.List[tp.List[str]]) -> str:
         list_map.append("".join(i) + "\n")
     return "".join(list_map)
 
+
 if __name__ == "__main__":
     solution = find_path(load_map())
     print(reconstruct_map(solution))
-
